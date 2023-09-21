919790721109985c268f072317543e62364e569e
Rahul Singh <145421114+sindhwalrahul7457@users.noreply.github.com>
2023-09-21 21:58:40 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,13 @@
+#Q2. Given are some following variables containing data:
+#(i) var1 = ‘ ‘
+#(ii) var2 = ‘[ DS , ML , Python]’
+#(iii) var3 = [ ‘DS’ , ’ML’ , ‘Python’ ]
+#(iv) var4 = 1.
+#What will be the data type of the above given variable.
+
+
+var1 = ' ' #string
+var2 = '[ DS , ML , Python]'  #string
+var3 = ['DS' , 'ML', 'Python']  #list
+var4 = 1            #int
+print(f"type of var1 = {type(var1)}\n data type of var 2 = {type(var2)} \nData type of var3 = {type(var3)}\nData type of var4 = {type(var4)}")
