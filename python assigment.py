a00f9f70d6e56906063189b4012e6b1c4ca8b903
Rahul Singh <145421114+sindhwalrahul7457@users.noreply.github.com>
2023-09-21 21:36:14 +0530

    Add files via upload

---
+++ python assigment.py
@@ -0,0 +1,19 @@
+#Q1. Create one variable containing following type of data:
+#(i) string
+#(ii) list
+#(iii) float
+#(iv) tuple
+name = 'rahul','singh','geeta','ram'
+print(name)
+list_1 = [1,2,3,43,6,True,'rahul',[1,3,4]]
+print(list_1)
+float_num = 4.5
+print(float_num)
+print(type(float_num))
+tuple_1 = (12,34,'rahul',True,44.5)
+print(tuple_1)
+print(type(tuple_1))
+final_list = ['rahul',[12,44.6,'rahul'],74.6,('rahul',45,65)]
+print(final_list)
+print(type(final_list))
+print(type(final_list[3]))
