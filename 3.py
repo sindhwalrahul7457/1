7dee1f6dae2bd87740510a28bae2e2a4e91bb637
Rahul Singh <145421114+sindhwalrahul7457@users.noreply.github.com>
2023-09-22 10:32:14 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,13 @@
+#Q3. Explain the use of the following operators using an example:
+#(i) /                        division operator. it gives answer in float
+#(ii) %                       modulo operator. it give us the remainder
+#(iii) //                     division operator. it give output in an integer value
+#(iv) **                      power operator. it gives the power of input item
+
+#       EXAMPLE............
+a = 5
+b = 2
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
