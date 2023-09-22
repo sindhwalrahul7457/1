9f025ab6460e28e4a001688d0e129154779a46d6
Rahul Singh <145421114+sindhwalrahul7457@users.noreply.github.com>
2023-09-22 11:05:07 +0530

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,10 @@
+#   Q7. What do you understand about mutable and immutable data types? Give examples for both showing
+# this property.
+
+# MUTABLE = mutable meaning is you can assign or overight value over the previous value. you can use assign(=) operator.
+# for example lis
+# IMMUTABLE = you cnat overight in immutable type of datatype or you cant modified it its unchangable. if you would
+# new value over previuos value it gives you type error for example tuple , set are immutable data type in python.
+list_1 = [1,3,5,6]
+list_1[2] = 10
+print(list_1)
