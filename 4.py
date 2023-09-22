1fbec1dedbaadb6a1de386fba4da9b00fc4d5e09
Rahul Singh <145421114+sindhwalrahul7457@users.noreply.github.com>
2023-09-22 10:38:23 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,7 @@
+#Q4.     Create a list of length 10 of your choice containing multiple types of data. Using for loop print the
+#        element and its data type.
+
+list_item = [1,35.6,"rahul",True,[1,5],{23,56},(24,67),56,"akash"]
+for i in range(10):
+    print(list_item[i])
+    print(type(list_item[i]))
